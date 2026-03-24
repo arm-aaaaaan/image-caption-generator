962ae28080185f9a024220b2ddea8d7d76d0a874
Arman Sharma <sharmaarman3256@gmail.com>
2026-03-24 19:25:24 +0530

    Refactored code: moved model to model.py and created train.py

--- train.py
+++ train.py
@@ -1,31 +1,8 @@
-import torch
-import torch.nn as nn
-import torchvision.models as models
 
-class EncoderCNN(nn.Module):
-    def __init__(self, embed_size, train_CNN=False):
-        super(EncoderCNN, self).__init__()
-        self.train_CNN=train_CNN
-        self.inception=models.inception_v3(pretrained=True, aux_logits=False)
-        self.inception.fc=nn.Linear(self.inception.fc.in_features,embed_size)
-        self.relu=nn.ReLU()
-        self.dropout=nn.Dropout(0.5)
+
+
+
     
-    def forward(self, images):
-        features=self.inception(images)
+                               
 
-        for name,parm in self.inception.named_parameters():
-            if "fc.weight" in name or "fc.bias" in name:
-                param.requires_grad=True
-            else:
-                param.requires_grad=self.train_CNN
         
-        return self.dropout(self.relu(features))
-    
-class DecoderRNN(nn.Module):
-    def __init__(self, embed_size, hidden_size, vocab_size, num_layers):
-        super(DecoderRNN,self).__init__()
-        self.embed=nn.Embedding(vocab_size,embed_size)
-        self.lstm=nn.LSTM(embed_size,hidden_size,num_layers)
-        self.linear=nn.Linear(hidden_size,vocab_size)
-        self.dropout=nn.Dropout(0.5)
